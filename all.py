dcc6db7f4af490d9e480ed1aa8601df19f7bd722
akhilaakhi27 <96654491+akhilaakhi27@users.noreply.github.com>
2024-02-01 13:09:33 +0530

    Update all.py

--- all.py
+++ all.py
@@ -7,7 +7,16 @@ max=b[0]
 for i in b:
     if i>max:
         max=i
-print(max)
+print(max)#to find largest no
+
+
+b=[2,4,6,7,9,5]
+min=b[0]
+for i in b:
+    if i<min:
+        min=i
+print(min)#to find smallest no
+
     
 
 
