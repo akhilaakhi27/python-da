72ca9d127772ae263ad394e85d1ca4f6fd96bbc4
akhilaakhi27 <96654491+akhilaakhi27@users.noreply.github.com>
2024-02-01 13:06:29 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,11 @@
+a={"Name":"Akhila","Age":24}
+for i in a:
+    print(i)
+    print(a[i])
+    
+for x in a.keys():
+    print(x)
+for x in a.values():
+    print(x)
+for x,y in a.items():
+    print(x,y)
---
+++ for.py
@@ -0,0 +1,46 @@
+a=[1,2,3,4,5]
+for i in a:
+     print(i)
+
+for i in a:
+    print(i)
+    if i>2:
+        break
+
+for i in a:
+    if i==3:
+        continue
+    print(i)
+
+x=1
+while x<10:
+    print(x)
+    x+=1
+    
+x=1
+while x<5:
+    print(x)
+    if x==3:
+      break
+    x+=1
+
+x=0
+while x<5:
+    x+=1
+    if x==3:
+      continue
+    print(x)
+    
+
+    
+    
+
+
+
+
+
+
+
+
+
+    
