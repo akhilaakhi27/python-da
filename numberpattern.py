4875eec4f7f75508c052618c23505d6e8c4ae84a
akhilaakhi27 <96654491+akhilaakhi27@users.noreply.github.com>
2024-02-03 12:47:02 +0530

    Add files via upload

---
+++ numberpattern.py
@@ -0,0 +1,38 @@
+row=6
+for i in range(row):
+    for j in range(i):
+      print(i,end=" ")
+    print(" ")
+    
+    
+row=6
+for i in range(row):
+    for j in range(1,i+1):
+     print(j,end=" ")
+    print(" ")
+    
+row=6
+for i in range(row):
+    for j in range(i):
+        print("*",end=" ")
+    print(" ")
+    
+s="main"
+if s==s[::-1]:
+    print("palindrome")
+else:
+    print("not palindrome")
+    
+
+
+
+    
+
+     
+    
+
+
+    
+
+    
+ 
