fcf3ac2c24824c441d26ea75f3645c252181d90e
akhilaakhi27 <96654491+akhilaakhi27@users.noreply.github.com>
2024-02-06 13:12:20 +0530

    Add files via upload

---
+++ fntsk.py
@@ -0,0 +1,35 @@
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+def sum1():
+    return a+b
+print(sum1())
+
+a=[1,2,3,4]
+def sum2(x):
+    s=0
+    for i in x:
+        s=s+i
+    print(s)
+sum2(a)
+
+b=[2,3,4]
+def mul(x):
+    m=1
+    for i in x:
+        m=m*i
+    print(m)
+mul(b)
+
+
+
+
+
+    
+    
+
+
+    
+    
+
+        
+    
