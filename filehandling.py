a9d3eb66b0e245e325169064ca714ac9a58e8102
akhilaakhi27 <96654491+akhilaakhi27@users.noreply.github.com>
2024-02-10 12:33:51 +0530

    Add files via upload

---
+++ filehandling.py
@@ -0,0 +1,40 @@
+# 4 modes for file opening
+# read  r mode
+# append a mode modification
+# write w mode overrite
+# x mode-Create file
+
+#1 read mode
+f=open("newfile.txt","r")
+# print(f.read())
+# print(f.read(5))
+# print(f.readline())
+# print(f.readline())
+# print(f.readline())
+
+# for i in f:
+#     print(i)
+f.close()
+
+#2 append mode
+f=open("newfile.txt","a")
+# f.write("modify file")
+f.close()
+f=open("newfile.txt","r")
+print(f.read())
+f.close()
+
+f=open("newfile.txt","w")
+# f.write("welcome to python")
+f.close()
+f=open("newfile.txt","r")
+print(f.read())
+
+
+
+
+
+
+
+
+
