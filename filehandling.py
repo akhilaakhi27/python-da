8f94390b2805a7fb4b22a4ae3b2ecd2352f0a761
akhilaakhi27 <96654491+akhilaakhi27@users.noreply.github.com>
2024-02-28 12:37:43 +0530

    Update filehandling.py

--- filehandling.py
+++ filehandling.py
@@ -29,6 +29,28 @@ f=open("newfile.txt","w")
 f.close()
 f=open("newfile.txt","r")
 print(f.read())
+import os
+# a=open("hello.txt","x")
+a=open("hello.txt","w")
+a.write("hello world,welcome to python")
+a=open("hello.txt","r")
+print(a.read())
+a.close()
+a=open("hello.txt","a")
+a.write("file handling")
+a.close()
+a=open("hello.txt","r")
+print(a.read())
+a.close()
+
+# b=open("hi.txt","x")
+# os.remove("hi.txt")
+
+os.rmdir("pythonn")
+
+
+
+
 
 
 
